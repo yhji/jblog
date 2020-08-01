cb378ae63620ef8762bb736eb16647a788957479
jyh <jyh@210.121.176.200>
2020-08-01 14:56:32 +0900

    init commit

---
+++ src/main/java/com/home/jblog/JblogApplication.java
@@ -0,0 +1,13 @@
+package com.home.jblog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JblogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JblogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/home/jblog/JblogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.home.jblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JblogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
